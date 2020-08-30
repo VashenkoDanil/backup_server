987099d465771b42aafde78092a7e074441c975d
Vashenko Danil <danil.vashenko.94@mail.ru>
2020-08-31 01:04:06 +0400

    Переделан импорт настроек из .env файла

--- settings/project.py
+++ settings/project.py
@@ -1,3 +1,8 @@
-ID_ROOT_DIRECTORY = 'id папки на гугл диске куда будет загружатся бэкап'
-FILE_PATH = 'путь до файла, который нужно загрузить на гугл диск'
-MAX_COUNT_BACKUP = 3
+from dotenv import find_dotenv, load_dotenv
+import os
+
+load_dotenv(dotenv_path=find_dotenv(), verbose=False, override=True)
+
+ID_ROOT_DIRECTORY = os.getenv('ID_ROOT_DIRECTORY')
+FILE_PATH = os.getenv('FILE_PATH')
+MAX_COUNT_BACKUP = int(os.getenv('MAX_COUNT_BACKUP'))
